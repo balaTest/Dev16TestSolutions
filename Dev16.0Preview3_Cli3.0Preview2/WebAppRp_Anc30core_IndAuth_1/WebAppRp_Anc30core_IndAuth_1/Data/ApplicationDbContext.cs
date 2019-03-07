4b2a67ca2c0d40adcafee308e7ebe311b0116062
Bala Chirtsabesan <balaTest@live.com>
2019-03-07 05:15:42 -0800

    [WebAppRp_Anc30core_IndAuth_1] Created solution

---
+++ Dev16.0Preview3_Cli3.0Preview2/WebAppRp_Anc30core_IndAuth_1/WebAppRp_Anc30core_IndAuth_1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppRp_Anc30core_IndAuth_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
