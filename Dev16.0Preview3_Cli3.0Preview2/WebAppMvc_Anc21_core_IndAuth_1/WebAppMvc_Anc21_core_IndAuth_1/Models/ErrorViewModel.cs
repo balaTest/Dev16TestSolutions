53aee345bd667ce41d1ac9ddf83cecf0497e8371
Bala Chirtsabesan <balaTest@live.com>
2019-03-07 05:24:01 -0800

    [WebAppMvc_Anc21_core_IndAuth_1] Created solution

---
+++ Dev16.0Preview3_Cli3.0Preview2/WebAppMvc_Anc21_core_IndAuth_1/WebAppMvc_Anc21_core_IndAuth_1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppMvc_Anc21_core_IndAuth_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
