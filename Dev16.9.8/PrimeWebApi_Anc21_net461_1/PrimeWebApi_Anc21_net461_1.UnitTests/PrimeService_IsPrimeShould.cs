2dfb7d295ae65c121bd02967e842cfd0ab4feee5
Bala Chirtsabesan <balaTest@live.com>
2021-06-28 09:15:21 -0700

    [PrimeWebApi_Anc21_net461_1] Created solution with Web API template and MSTest project running on .NET Framework

---
+++ Dev16.9.8/PrimeWebApi_Anc21_net461_1/PrimeWebApi_Anc21_net461_1.UnitTests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeWebApi_Anc21_net461_1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWebApi_Anc21_net461_1.UnitTests
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, $"1 should not be prime");
+        }
+    }
+}
---
+++ Dev16.9.8/PrimeWebApi_Anc21_net461_1/PrimeWebApi_Anc21_net461_1.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimeWebApi_Anc21_net461_1.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeWebApi_Anc21_net461_1.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4ed0995-1421-42a7-8b43-5d243045b72b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
